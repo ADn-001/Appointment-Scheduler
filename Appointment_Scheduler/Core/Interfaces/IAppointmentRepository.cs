e98a4cbb5ce4f3410b48526af8e08a5c32e096cf
alex111.sebastian <alex111.sebastian@LAPTOP-58QD9NNA>
2025-04-23 18:52:00 +0400

    commit by alex

---
+++ Appointment_Scheduler/Core/Interfaces/IAppointmentRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System;
+using Core.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IAppointmentRepository
+    {
+        void Add(Appointment appointment);
+        Appointment GetById(Guid id);
+        IEnumerable<Appointment> GetAll();
+        void Remove(Guid id);
+    }
+}
