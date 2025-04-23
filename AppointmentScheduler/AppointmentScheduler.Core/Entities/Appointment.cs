051a59e16b273a4a80aad015c518c6a95ddcc12d
ADn-001 <99028978+ADn-001@users.noreply.github.com>
2025-04-23 21:09:33 +0400

    commit by Adnan

---
+++ AppointmentScheduler/AppointmentScheduler.Core/Entities/Appointment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace AppointmentScheduler.Core.Entities
+{
+    public class Appointment
+    {
+        public Guid Id { get; private set; }
+        public string Title { get; private set; }
+        public DateTime DateTime { get; private set; }
+
+        public Appointment(string title, DateTime dateTime)
+        {
+            if (dateTime < DateTime.Now)
+                throw new ArgumentException("Cannot schedule an appointment in the past.");
+
+            Id = Guid.NewGuid();
+            Title = title;
+            DateTime = dateTime;
+        }
+    }
+}
---
+++ AppointmentScheduler/AppointmentScheduler.Web/Dtos/AppointmentDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AppointmentScheduler.Web.Dtos
+{
+    public class AppointmentDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
